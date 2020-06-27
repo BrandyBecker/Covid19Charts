85afc6c88d606eff434b474702e5bbb22523552c
BrandyBecker <brandybeckerfsw@gmail.com>
2020-06-27 11:04:44 -0400

    updates React App name and favicon and desc

---
+++ covid/src/components/chart.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function Chart() {
+    return (
+      <div className="App">
+         <div> Graph </div>
+      </div>
+    );
+  }
+  
+  export default Chart;
+  
